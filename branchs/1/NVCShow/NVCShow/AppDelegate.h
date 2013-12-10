f26d2dbe2a0d80390a6e60743ba0debb6d81abf3
TianzhuChen <zhuzismart@163.com>
2013-12-10 16:45:03 +0800

    Initial Commit

---
+++ branchs/1/NVCShow/NVCShow/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  NVCShow
+//
+//  Created by Iwinad_CTZ on 12/10/13.
+//  Copyright (c) 2013 CTZ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+@end
---
+++ branchs/one/NVCShow/NVCShow/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  NVCShow
+//
+//  Created by Iwinad_CTZ on 12/10/13.
+//  Copyright (c) 2013 CTZ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
